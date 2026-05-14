d5f032ac2819f8433d93ea09e317a3b1966b614e
Mike Conrad <34722899+enxoco@users.noreply.github.com>
2026-05-14 13:02:19 -0400

    linting errors

--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -33,8 +33,6 @@ router
       .prefix('account')
       .as('profile')
       .use(middleware.auth())
-      
   })
-    
   .prefix('/api/v1')
 router.get('/users', [controllers.UsersLists, 'show']).prefix('/api/v1')
